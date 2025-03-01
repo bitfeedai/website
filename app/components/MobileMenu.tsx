ab1ac969f14bd79d50feed459f8ad264896d5040
unameit10000000 <free4ms@gmail.com>
2025-03-01 23:49:43 +0100

    Added padding top

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -28,7 +28,7 @@ export function MobileMenu() {
           <span className="sr-only">Toggle menu</span>
         </Button>
       </SheetTrigger>
-      <SheetContent side="right" className="w-[300px] sm:w-[400px]">
+      <SheetContent side="right" className="pt-12 w-[300px] sm:w-[400px]">
         <nav className="flex flex-col gap-4">
           {navItems.map((item, index) => (
             <Link
