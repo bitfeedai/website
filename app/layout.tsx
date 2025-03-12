14618fa0e816c9d9982879e0f7a8c74391c3e188
unameit10000000 <free4ms@gmail.com>
2025-03-12 21:25:42 +0100

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type React from "react"
-import "./globals.css"
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import type { Metadata } from "next"
 import { ScrollToTop } from "./components/ScrollToTop"
+import Head from "next/head"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,10 +20,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={inter.className}>{children}</body>
       <ScrollToTop />
     </html>
   )
 }
-
-import './globals.css'
